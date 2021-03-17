c4c6742f0197410a64386206af97c87e3953049d
Eric Triebe <eric.triebe@gmail.com>
2021-03-17 08:54:01 -0700

    Delete unneeded obj files

--- src/Program.cs
+++ src/Program.cs
@@ -16,6 +16,7 @@ namespace betterclipboard
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
